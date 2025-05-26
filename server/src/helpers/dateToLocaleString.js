6f5b61c749c5053f431e2d5f7c389dbeff5d18c6
MuhteremKuzucu <kuzucumuhterem@gmail.com>
2025-05-26 13:04:29 +0200

    folder structure

---
+++ server/src/helpers/dateToLocaleString.js
@@ -0,0 +1,11 @@
+"use strict"
+/* -------------------------------------------------------
+                Event Project
+------------------------------------------------------- */
+// dateToLocaleString(date:Date):
+
+module.exports = function (dateData) {
+    return dateData.toLocaleString('tr-tr', { dateStyle: 'full', timeStyle: 'medium' })
+}
+
+// 26 mart Perşembe 2025   14.19
