eb0a2d3a9f635571ec77462297d327706d201d73
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-07-16 16:52:45 +0200

    added image variable

--- server/src/controllers/event.js
+++ server/src/controllers/event.js
@@ -9,152 +9,153 @@ const Group = require("../models/group");
 const Category = require("../models/category");
 
 module.exports = {
-  list: async (req, res) => {
-    /*
-        #swagger.tags=["Events"]
-        #swagger.summary="List Events"
-        #swagger.description=`
-         You can send query with endpoint for filter[], search[], sort[], page and limit.
-                <ul> Examples:
-                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
-                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
-                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
-                    <li>URL/?<b>page=2&limit=1</b></li>
-                </ul>`      
-        
-    */
+    list: async (req, res) => {
+        /*
+            #swagger.tags=["Events"]
+            #swagger.summary="List Events"
+            #swagger.description=`
+             You can send query with endpoint for filter[], search[], sort[], page and limit.
+                    <ul> Examples:
+                        <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                        <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                        <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                        <li>URL/?<b>page=2&limit=1</b></li>
+                    </ul>`      
+            
+        */
 
-    const result = await res.getModelList(Event);
+        const result = await res.getModelList(Event);
 
-    res.status(200).send({
-      error: false,
-      details: await res.getModelListDetails(Event),
-      result,
-    });
-  },
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(Event),
+            result,
+        });
+    },
 
-  create: async (req, res) => {
-    /* 
-        #swagger.tags=["Events"]
-        #swagger.summary="Create Events"
-        #swagger.parameters['body']={
-            in:"body",
-            require:true,
-            schema:{
-             $ref:'#/definitions/Event',
-
+    create: async (req, res) => {
+        /* 
+            #swagger.tags=["Events"]
+            #swagger.summary="Create Events"
+            #swagger.parameters['body']={
+                in:"body",
+                require:true,
+                schema:{
+                 $ref:'#/definitions/Event',
+    
+                }
+            
             }
-        
-        }
-        
-    */
+            
+        */
 
-    const participantInfos = req.body.participants || [];
-    const groupInfos = req.body.sharedGroup || [];
+        const participantInfos = req.body.participants || [];
+        const groupInfos = req.body.sharedGroup || [];
 
-    const participantIds = [];
-    const groupIds = [];
+        const participantIds = [];
+        const groupIds = [];
 
-    let user, group;
+        let user, group;
 
-    for (let info of participantInfos) {
-      if (typeof info === "object") {
-        user = await User.findOne({ email: info.email });
-        if (!user) {
-          user = await User.create({ ...info });
-        }
-      } else if (typeof info === "string") {
-        user = await User.findById(info);
-      }
-
-      if (user) participantIds.push(user._id);
-    }
-
-    for (let info of groupInfos) {
-      if (typeof info === "object") {
-        group = await Group.findOne({ title: info.title });
-        if (!group) {
-          group = await Group.create({ ...info, creater: req.user._id });
-        }
-      } else if (typeof info === "string") {
-        group = await Group.findById(info);
-      }
-
-      if (group) groupIds.push(group._id);
-    }
-
-    const result = await Event.create({
-      creater: req.user._id,
-      participants: participantIds,
-      sharedGroup: groupIds,
-      categoryId: req.body.categoryId,
-      title: req.body.title,
-      description: req.body.description,
-      date: req.body.date,
-      time: req.body.time,
-      location: req.body.location,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-
-    res.status(201).send({
-      error: false,
-      message: "Event created successfully",
-      result,
-    });
-  },
-
-  read: async (req, res) => {
-    /*
-     #swagger.tags=["Events"]
-     #swagger.summary="Read Event"
-    */
-
-    const result = await Event.findOne({ _id: req.params.id }).populate([
-      { path: "creater", select: "username email" },
-      { path: "participants", select: "firstName lastName email" },
-      { path: "sharedGroup", select: "title location" },
-      { path: "categoryId", select: "name" },
-    ]);
-
-    res.status(200).send({
-      error: false,
-      result,
-    });
-  },
-
-  update: async (req, res) => {
-    /* 
-        #swagger.tags=["Events"]
-        #swagger.summary="Update Events"
-        #swagger.parameters['body']={
-            in:"body",
-            require:true,
-            schema:{
-              $ref:'#/definitions/Event',
+        for (let info of participantInfos) {
+            if (typeof info === "object") {
+                user = await User.findOne({ email: info.email });
+                if (!user) {
+                    user = await User.create({ ...info });
+                }
+            } else if (typeof info === "string") {
+                user = await User.findById(info);
             }
-        
+
+            if (user) participantIds.push(user._id);
         }
-        
-    */
-   
-    const result = await Event.updateOne({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true,
-    });
 
-    res.status(202).send({
-      error: false,
-      result,
-    });
-  },
+        for (let info of groupInfos) {
+            if (typeof info === "object") {
+                group = await Group.findOne({ title: info.title });
+                if (!group) {
+                    group = await Group.create({ ...info, creater: req.user._id });
+                }
+            } else if (typeof info === "string") {
+                group = await Group.findById(info);
+            }
 
-  deletee: async (req, res) => {
-    const result = await Event.deleteOne({ _id: req.params.id });
+            if (group) groupIds.push(group._id);
+        }
 
-    res.status(result.deletedCount ? 204 : 404).send({
-      error: !result.deletedCount,
-      message: "Event is not found or already deleted",
-    });
-  },
+        const result = await Event.create({
+            creater: req.user._id,
+            participants: participantIds,
+            sharedGroup: groupIds,
+            categoryId: req.body.categoryId,
+            title: req.body.title,
+            description: req.body.description,
+            date: req.body.date,
+            time: req.body.time,
+            location: req.body.location,
+            image: req.body.image,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        });
+
+        res.status(201).send({
+            error: false,
+            message: "Event created successfully",
+            result,
+        });
+    },
+
+    read: async (req, res) => {
+        /*
+         #swagger.tags=["Events"]
+         #swagger.summary="Read Event"
+        */
+
+        const result = await Event.findOne({ _id: req.params.id }).populate([
+            { path: "creater", select: "username email" },
+            { path: "participants", select: "firstName lastName email" },
+            { path: "sharedGroup", select: "title location" },
+            { path: "categoryId", select: "name" },
+        ]);
+
+        res.status(200).send({
+            error: false,
+            result,
+        });
+    },
+
+    update: async (req, res) => {
+        /* 
+            #swagger.tags=["Events"]
+            #swagger.summary="Update Events"
+            #swagger.parameters['body']={
+                in:"body",
+                require:true,
+                schema:{
+                  $ref:'#/definitions/Event',
+                }
+            
+            }
+            
+        */
+
+        const result = await Event.updateOne({ _id: req.params.id }, req.body, {
+            new: true,
+            runValidators: true,
+        });
+
+        res.status(202).send({
+            error: false,
+            result,
+        });
+    },
+
+    deletee: async (req, res) => {
+        const result = await Event.deleteOne({ _id: req.params.id });
+
+        res.status(result.deletedCount ? 204 : 404).send({
+            error: !result.deletedCount,
+            message: "Event is not found or already deleted",
+        });
+    },
 };
--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -6,95 +6,94 @@
 const User = require("../models/user");
 
 module.exports = {
-  list: async (req, res) => {
-    /*
-            #swagger.tags = ["Users"]
-            #swagger.summary = "List Users"
-            #swagger.description = `
-                You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
-                <ul> Examples:
-                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
-                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
-                    <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
-                    <li>URL/?<b>limit=10&page=1</b></li>
-                </ul>
-            `
-        */
+    list: async (req, res) => {
+        /*
+                #swagger.tags = ["Users"]
+                #swagger.summary = "List Users"
+                #swagger.description = `
+                    You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
+                    <ul> Examples:
+                        <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                        <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                        <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
+                        <li>URL/?<b>limit=10&page=1</b></li>
+                    </ul>
+                `
+            */
 
-    const result = await res.getModelList(User);
+        const result = await res.getModelList(User);
 
-    res.status(200).send({
-      error: false,
-      details: await res.getModelListDetails(User),
-      result,
-    });
-  },
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(User),
+            result,
+        });
+    },
 
-  read: async (req, res) => {
-    /*
-            #swagger.tags = ["Users"]
-            #swagger.summary = "Get Single User"
-        */
+    read: async (req, res) => {
+        /*
+                #swagger.tags = ["Users"]
+                #swagger.summary = "Get Single User"
+            */
 
-    const result = await User.findById(req.params.id);
+        const result = await User.findById(req.params.id);
 
-    res.status(200).send({
-      error: false,
-      result,
-    });
-  },
+        res.status(200).send({
+            error: false,
+            result,
+        });
+    },
 
-  update: async (req, res) => {
-    /*
-            #swagger.tags = ["Users"]
-            #swagger.summary = "Update User"
-            #swagger.parameters['body'] = {
-                in: 'body',
-                required: true,
-                schema: {
-                    "username": "test",
-                    "password": "1234",
-                    "email": "test@site.com",
-                    "firstName": "test",
-                    "lastName": "test",
+    update: async (req, res) => {
+        /*
+                #swagger.tags = ["Users"]
+                #swagger.summary = "Update User"
+                #swagger.parameters['body'] = {
+                    in: 'body',
+                    required: true,
+                    schema: {
+                        "username": "test",
+                        "password": "1234",
+                        "email": "test@site.com",
+                        "firstName": "test",
+                        "lastName": "test",
+                    }
                 }
-            }
-        */
-   
-    console.log(req.body);
-            if (req.user._id.toString() !== req.params.id.toString()) {
-      throw new Error("You can not update someone else profıle");
-    }
-    const result = await User.updateOne({ _id: req.params.id }, req.body, {
-      runValidators: true,
-    });
+            */
 
-    res.status(200).send({
-      error: false,
-      user: await User.findById(req.params.id),
-    });
-  },
+        if (req.user._id.toString() !== req.params.id.toString()) {
+            throw new Error("You can not update someone else profile");
+        }
+        const result = await User.updateOne({ _id: req.params.id }, req.body, {
+            runValidators: true,
+        });
 
-  deletee: async (req, res) => {
-    /*
-            #swagger.tags = ["Users"]
-            #swagger.summary = "Delete User"
-        */
+        res.status(200).send({
+            error: false,
+            user: await User.findById(req.params.id),
+        });
+    },
 
-    if (req.user.id !== req.params.id.toString()) {
-        throw new Error("You can only delete your own profile");
-      
-    };
-    
+    deletee: async (req, res) => {
+        /*
+                #swagger.tags = ["Users"]
+                #swagger.summary = "Delete User"
+            */
 
-    const result = await User.deleteOne({ _id: req.params.id });
+        if (req.user.id !== req.params.id.toString()) {
+            throw new Error("You can only delete your own profile");
 
-    res.status(result.deletedCount ? 204 : 404).send({
-      error: !result.deletedCount,
-      message: result.deletedCount
-        ? "Data deleted."
-        : "Data is not found or already deleted.",
-      result,
-    });
-  },
+        };
+
+
+        const result = await User.deleteOne({ _id: req.params.id });
+
+        res.status(result.deletedCount ? 204 : 404).send({
+            error: !result.deletedCount,
+            message: result.deletedCount
+                ? "Data deleted."
+                : "Data is not found or already deleted.",
+            result,
+        });
+    },
 };
